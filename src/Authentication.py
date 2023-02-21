6422744b8f94ee2cb4ec23e4a7535f3daad8be0b
Tristan Larkin <trlarkin@unm.edu>
2023-02-21 15:03:44 -0700

    adden basic authenticate() function

--- src/Authentication.py
+++ src/Authentication.py
@@ -1,6 +1,14 @@
+import json
+
 class Authentification():
     def __init__(self, storage_file:str) -> None:
         self.storage_file = storage_file
 
     def authenticate(code:list) -> bool:
-        
+        with open('sample.json', 'r') as openfile:
+            json_object = json.load(openfile)
+            if json_object["2-factor"]:
+                pass
+            else:
+                if json["passcode"] == code:
+                    return True

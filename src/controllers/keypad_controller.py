ff202f2eb0fcb629a09e86d931ff1b3459b542ca
Brian McCollum <bmccollum00@gmail.com>
2023-02-23 14:57:06 -0700

    working on sound playing

--- src/controllers/keypad_controller.py
+++ src/controllers/keypad_controller.py
@@ -2,7 +2,7 @@ from models import Odin
 from controllers import main_controller, output_controller
 
 def input_char(input: str) -> None:
-    output_controller.play_tone()
+    output_controller.play_tone(7, output_controller.sounds.SHORT)
     if len(Odin.current_code) < 12:
         Odin.current_code += input
     print(Odin.current_code)
--- src/controllers/output_controller.py
+++ src/controllers/output_controller.py
@@ -1,18 +1,22 @@
+import os
 from enum import Enum
 import playsound
 
+
 class sounds(Enum):
-    SHORT = "resources/shortbeep.wav"
+    # SHORT = "resources/shortbeep.mp3"
+    SHORT = "./resources/shortbeep.mp3"
     LONG = "resources/longbeep.wav"
 
 class colors(Enum):
     RED = "RED"
 
-def play_tone(volume: int, type: sounds) -> bool:
+def play_tone(volume: int, tone: sounds) -> bool:
     try:
-        playsound.playsound("resources/shortbeep.wav")
+        playsound.playsound(tone)
         return True
     except:
+        print("failed to play sound")
         return False
 
 def led_feedback(length: int, color: colors) -> bool:

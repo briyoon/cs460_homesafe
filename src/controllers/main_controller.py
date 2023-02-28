185905c3329585b6e7e466eafbab65923b653349
Tristan Larkin <trlarkin@unm.edu>
2023-02-27 19:44:42 -0700

    added the code to handle command

--- src/controllers/main_controller.py
+++ src/controllers/main_controller.py
@@ -1,4 +1,7 @@
-def handle_command(command: int):
+from controllers import storage_controller as sc
+from controllers.storage_controller import Settings
+
+def handle_command(command: str):
     """
     Upon receiving the command from the Authentication object, this procedure
     will be called with the received command as the parameter. The actual
@@ -9,6 +12,17 @@ def handle_command(command: int):
         command (int): _description_
     """
     match command:
-
-        case _: # deafult (invalid)
+        case "*990":
+            sc.write_file(Settings.VOLUME, 0)
+        case "*991":
+            sc.write_file(Settings.VOLUME, 1)
+        case "*992":
+            sc.write_file(Settings.VOLUME, 2)
+        case "*111":
+            sc.write_file(Settings.TWO_FACTOR, False)
+        case "*222":
+            sc.write_file(Settings.TWO_FACTOR, True)
+        case "*456":
             pass
+        case _: # deafult (invalid)
+            print("Invalid command")
